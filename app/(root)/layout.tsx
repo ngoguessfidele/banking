2ee0367cd4764f0619dd708eae229e4109359c94
ngoguessfidele <ngoguessfidele@gmail.com>
2024-05-16 13:47:02 +0200

    Implement complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,6 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
 
 
 
@@ -7,10 +10,25 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Adrian', lastName: 'JSM'}
+
   return (
-    <main>
-        SIDEBAR
-        {children}
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn} />
+
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image 
+              src="/icons/logo.svg" width={30} height={30} alt="logo"
+            />
+            <div>
+              <MobileNav user={loggedIn}
+              />
+            </div>
+          </div>
+          {children}
+        </div>
+        
     </main>
   );
 }
